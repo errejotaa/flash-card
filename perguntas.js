890f7a32bcb5f13e4be26eb91b7e91e190195622
errejotaa <00001120666156sp@al.educacao.sp.gov.br>
2024-11-01 13:50:37 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'História',
+    'Onde ocorreu a Guerra de Canudos e quem foi o líder?',
+    'A Guerra de Canudos ocorreu em Canudos, no interior da Bahia, e foi liderada por Antônio Conselheiro'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Canadá?',
+    'A capital do Canadá é Ottawa'
+)
+
+criaCartao(
+    'Literatura',
+    'Qual foi a primeira obra realista brasileira?',
+    'A primeira obra realista brasileira foi o livro machadiano: Memórias Póstumas de Brás Cubas'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual o passado particípio do verbo go?',
+    'O passado particípio do verbo go é gone'
+)
+criaCartao(
+    'Matemática',
+    'Qual a variável mais utilizada nas contas matemáticas?',
+    'A variável mais utilizada nas contas matemáticas é o "X"'
+)
+criaCartao(
+    'Química',
+    'Qual a composição química do cloreto de sódio?',
+    'A composição química do cloreto de sódio é "NaCl"'
+)
